05dd7700f746a4ab775539935e19907ae3bc56ed
blakereeves19 <blake.reeves19@gmail.com>
2019-02-25 14:09:36 -0600

    Completed lab

--- App.tsx
+++ App.tsx
@@ -9,23 +9,39 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+interface IAppProps { }
+interface IAppState {
+  users: {id: number, name: string }[];
+}
 
 interface Props {}
-export default class App extends Component<Props> {
+export default class App extends Component<IAppProps, IAppState> {
+
+  constructor(props: IAppProps) {
+    super(props)
+    this.state = {
+      users: []
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      let res = await fetch('https://jsonplaceholder.typicode.com/users');
+      let users = await res.json();
+      this.setState({ users });
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {this.state.users.map(user => (
+          <Text key={user.id} style={styles.textStyle}>{user.name}</Text>
+        ))}
       </View>
     );
   }
@@ -38,14 +54,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#0091ea',
   },
-  welcome: {
+  textStyle: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    margin: 5,
+    color: 'white'
+  }
 });
